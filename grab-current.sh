295185e9c110c2e62a2b2ce54e387f3ff0104523
Frank Sievertsen <packaging@fx5.de>
2013-02-26 10:55:20 +0100

    set -e

--- grab-current.sh
+++ grab-current.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+set -e
 tomoyo-selectpolicy -r "<kernel> /usr/bin/firefox" </sys/kernel/security/tomoyo/domain_policy | tomoyo-sortpolicy >domain_policy
 egrep "/usr/bin/firefox|/usr/lib/firefox/firefox|lightdm-guest-session-wrapper" /sys/kernel/security/tomoyo/exception_policy >exception_policy
--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+set -e 
 tomoyo-loadpolicy -d <domain_policy
 tomoyo-loadpolicy -e <exception_policy
 
-echo "Don't forget to do # tomoyo-savepolicy"
+echo "Don't forget to run"
+echo " # tomoyo-savepolicy"
