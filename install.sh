7e7a1ba6a5f53e0621201326c91e6cd2bbcfa45e
Frank Sievertsen <packaging@fx5.de>
2013-02-26 11:35:17 +0100

    It doesn't hurt to save policy twice

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 set -e 
+
+tomoyo-savepolicy
 tomoyo-loadpolicy -d <domain_policy
 tomoyo-loadpolicy -e <exception_policy
-
-echo "Don't forget to run"
-echo " # tomoyo-savepolicy"
+tomoyo-savepolicy
